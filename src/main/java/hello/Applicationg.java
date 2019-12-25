0e57db4d50357814a1ede783aff044d75c2f32db
unknown <1978707987@qq.com>
2019-12-25 16:19:45 +0800

    connect to mysql by mybatis

--- src/main/java/hello/Applicationg.java
+++ src/main/java/hello/Applicationg.java
@@ -1,10 +1,33 @@
 package hello;
 
+import hello.dao.UserDOMapper;
+import hello.dataObject.UserDO;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.sql.Connection;
+import java.util.Properties;
 
-@SpringBootApplication
-public class Applicationg {
+@SpringBootApplication(scanBasePackages = {"hello"})
+@MapperScan("hello.dao")
+@RestController
+public class Applicationg{
+
+    @Autowired
+    private UserDOMapper userDOMapper;
+
+    @RequestMapping("/wjl")
+    public String wjl(){
+        UserDO userDO = userDOMapper.selectByPrimaryKey(1);
+        if(userDO == null){
+            return "not exist";
+        }else{
+            return userDO.getTelephone();
+        }
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Applicationg.class, args);
